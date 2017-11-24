f19f372ffcd4c27566d6be8c516f2dda16ec1691
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-11-24 17:48:08 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -28,7 +28,7 @@ class CashRegister
     end
 
     def void_last_transaction
-      self.total = self.total - self.last_transaction
+      self.total = self.total - self.last_transaction #So glad I can use self instead of CashRegister over and over
     end
 
 end
